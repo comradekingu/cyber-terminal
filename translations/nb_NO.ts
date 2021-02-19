6f318a85c867f294be27f767c2a58c21054b3bf2
omame <me@omame.tech>
2021-02-19 17:41:26 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/nb_NO/

--- translations/nb_NO.ts
+++ translations/nb_NO.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished">En prosess kjører. Er du sikker på at du vil avslutte?</translation>
+        <translation>En prosess kjører. Er du sikker på at du vil avslutte?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
@@ -21,11 +21,6 @@
 </context>
 <context>
     <name>Konsole::Vt102Emulation</name>
-    <message>
-        <location filename="../qmltermwidget/lib/Vt102Emulation.cpp" line="1257"/>
-        <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>QObject</name>
@@ -36,28 +31,13 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qmltermwidget/lib/ColorScheme.cpp" line="464"/>
-        <source>Accessible Color Scheme</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../qmltermwidget/lib/Filter.cpp" line="544"/>
         <source>Open Link</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qmltermwidget/lib/Filter.cpp" line="545"/>
-        <source>Copy Link Address</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../qmltermwidget/lib/Filter.cpp" line="549"/>
         <source>Send Email To...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qmltermwidget/lib/Filter.cpp" line="550"/>
-        <source>Copy Email Address</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
