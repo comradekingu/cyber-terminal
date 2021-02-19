c4061392b340b46cbae415c7450a73463dd85c89
ivan123 <anotherIVan123@outlook.com>
2021-02-19 11:59:56 +0000

    Translated using Weblate (Russian)
    Currently translated at 30.0% (3 of 10 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/ru/

--- translations/ru_RU.ts
+++ translations/ru_RU.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation>Процесс запущен, вы уверены, что хотите выйти?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>ОК</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отменить</translation>
     </message>
 </context>
 </TS>
