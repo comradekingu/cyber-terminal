815d13706c2c88733d642b35dc8be2c1e574d393
Allan Nordhøy <epost@anotheragency.no>
2021-02-19 16:49:11 +0100

    Added translation using Weblate (Norwegian Bokmål)

---
+++ translations/nb_NO.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>ExitPromptDialog</name>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
+        <source>Process is running, are you sure you want to quit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Konsole::Vt102Emulation</name>
+    <message>
+        <location filename="../qmltermwidget/lib/Vt102Emulation.cpp" line="1257"/>
+        <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../qmltermwidget/lib/ColorScheme.cpp" line="278"/>
+        <location filename="../qmltermwidget/lib/ColorScheme.cpp" line="293"/>
+        <source>Un-named Color Scheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qmltermwidget/lib/ColorScheme.cpp" line="464"/>
+        <source>Accessible Color Scheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qmltermwidget/lib/Filter.cpp" line="544"/>
+        <source>Open Link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qmltermwidget/lib/Filter.cpp" line="545"/>
+        <source>Copy Link Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qmltermwidget/lib/Filter.cpp" line="549"/>
+        <source>Send Email To...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qmltermwidget/lib/Filter.cpp" line="550"/>
+        <source>Copy Email Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
