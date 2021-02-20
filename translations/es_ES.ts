e991f04c06fb62e2674129cc006412fa4ae2b907
Anonymous <noreply@weblate.org>
2021-02-20 00:52:09 +0000

    Translated using Weblate (Spanish)
    Currently translated at 33.3% (1 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/es/

--- translations/es_ES.ts
+++ translations/es_ES.ts
@@ -11,12 +11,12 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">OK</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cancelar</translation>
     </message>
 </context>
 </TS>
