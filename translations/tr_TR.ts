726d02765215669e62f7c460952a70f33d769db7
Anonymous <noreply@weblate.org>
2021-02-27 14:40:34 +0000

    Translated using Weblate (Turkish)
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/tr/

--- translations/tr_TR.ts
+++ translations/tr_TR.ts
@@ -16,7 +16,7 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">İptal</translation>
     </message>
 </context>
 </TS>
