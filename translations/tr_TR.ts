d3569e7e1f3208dd4c4919209d8ef1f1e3ffd886
Aren Coşkun <ar3n1596@gmail.com>
2021-02-27 15:37:50 +0000

    Translated using Weblate (Turkish)
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/tr/

--- translations/tr_TR.ts
+++ translations/tr_TR.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation>İşlem hala çalışıyor, çıkmak istediğinize emin misiniz?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>Tamam</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
