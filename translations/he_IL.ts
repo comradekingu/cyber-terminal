e74cbe2e003d350806cb458d2cf4e76608245e61
TheShwarma <cube.lor.pit@gmail.com>
2021-04-15 11:14:02 +0000

    Translated using Weblate (Hebrew (Israel))
    Currently translated at 66.6% (2 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/he_IL/

--- translations/he_IL.ts
+++ translations/he_IL.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished">התהליך פועל, האם אתה בטוח שברצונך להפסיק?</translation>
+        <translation type="unfinished">תהליך פועל, האם אתם בטוחים שברצונכם להפסיק?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished">בסדר</translation>
+        <translation>אישור</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished">לְבַטֵל</translation>
+        <translation>ביטול</translation>
     </message>
 </context>
 </TS>
