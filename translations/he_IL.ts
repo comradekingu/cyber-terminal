629225bc4007bcaa7005bb2ed055e1640c5e2113
Anonymous <noreply@weblate.org>
2021-04-15 10:59:44 +0000

    Translated using Weblate (Hebrew (Israel))
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/he_IL/

--- translations/he_IL.ts
+++ translations/he_IL.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">התהליך פועל, האם אתה בטוח שברצונך להפסיק?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">בסדר</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">לְבַטֵל</translation>
     </message>
 </context>
 </TS>
