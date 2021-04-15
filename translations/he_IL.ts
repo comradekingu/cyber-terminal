18f8fbca434ab0b5eced1e032f250f58485926ab
Weblate <noreply@weblate.org>
2021-04-15 12:59:28 +0200

    Added translation using Weblate (Hebrew)

---
+++ translations/he_IL.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he">
+<context>
+    <name>ExitPromptDialog</name>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
+        <source>Process is running, are you sure you want to quit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
