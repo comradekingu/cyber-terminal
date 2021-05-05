11eb8e6f915884e4b385d871267101921990646b
Tameem Alshebel <tamoomy2004@gmail.com>
2021-05-05 12:59:02 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/ar/

--- translations/ar_AA.ts
+++ translations/ar_AA.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished">العملية جارية ، هل أنت متأكد أنك تريد الإقلاع عن التدخين؟</translation>
+        <translation>العملية جارية ، هل أنت متأكد أنك تريد الإقلاع؟</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished">موافق</translation>
+        <translation>موافق</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished">إلغاء</translation>
+        <translation>إلغاء</translation>
     </message>
 </context>
 </TS>
