993a8bd689bee2013401b96e48afc411d0737320
Anonymous <noreply@weblate.org>
2021-05-05 07:35:39 +0000

    Translated using Weblate (Malayalam)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/ml/

--- translations/ml_IN.ts
+++ translations/ml_IN.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">പ്രോസസ്സ് പ്രവർത്തിക്കുന്നു, നിങ്ങൾക്ക് പുറത്തുകടക്കണമെന്ന് ഉറപ്പാണോ?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ശരി</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">റദ്ദാക്കുക</translation>
     </message>
 </context>
 </TS>
