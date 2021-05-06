871d3538cd9adb3ac9ac640e16934c29a8a23284
Anonymous <noreply@weblate.org>
2021-05-06 02:34:17 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/id/

--- translations/id_ID.ts
+++ translations/id_ID.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Proses sedang berjalan, Anda yakin ingin keluar?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">baik</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Membatalkan</translation>
     </message>
 </context>
 </TS>
