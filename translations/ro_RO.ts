48663ba1ac600c767f85033f09b6d4dac1b91846
Anonymous <noreply@weblate.org>
2021-05-06 15:54:18 +0000

    Translated using Weblate (Romanian)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/ro/

--- translations/ro_RO.ts
+++ translations/ro_RO.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Procesul este în curs de desfășurare, sunteți sigur că doriți să renunțați?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">OK</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Anulează</translation>
     </message>
 </context>
 </TS>
