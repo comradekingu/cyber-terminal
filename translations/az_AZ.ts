9666a4a2bdb7fe955dea93b27194ba873c22e38a
Anonymous <noreply@weblate.org>
2021-05-17 18:19:43 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 0.0% (0 of 3 strings)
    
    Translation: CyberOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cyberos/terminal/az/

--- translations/az_AZ.ts
+++ translations/az_AZ.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Proses işləyir, çıxmaq istədiyinizə əminsiniz?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">tamam</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ləğv et</translation>
     </message>
 </context>
 </TS>
